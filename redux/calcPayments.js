a4e138a7df7acf39ad6244d2f9956a24340ab659
Koder Astoncat <koder@astoncat.com>
2022-08-28 21:00:13 +0200

    Refactoring of reducers code
    using Prettier

--- redux/calcPayments.js
+++ redux/calcPayments.js
@@ -1,66 +1,66 @@
 import axios from 'axios'
 
 export function showLoader() {
-    return {
-        type: "SHOW_LOADER"
-    }
-  }
+	return {
+		type: 'SHOW_LOADER'
+	}
+}
 
 export function hideLoader() {
-    return {
-        type: "HIDE_LOADER"
-    }
+	return {
+		type: 'HIDE_LOADER'
+	}
 }
 
 export function doCalc() {
-    return (dispatch, getState) => {
-        dispatch(showLoader())
-        const { amount } = getState()
-        dispatch(calc(amount.requested))
-        setTimeout(() => {
-            dispatch(hideLoader())
-        }, 1000)
-    }
+	return (dispatch, getState) => {
+		dispatch(showLoader())
+		const { amount } = getState()
+		dispatch(calc(amount.requested))
+		setTimeout(() => {
+			dispatch(hideLoader())
+		}, 1000)
+	}
 }
 
 export function calc(amount) {
-    return {
-        type: "CALC",
-        payload: amount
-    }
+	return {
+		type: 'CALC',
+		payload: amount
+	}
 }
 
 const initialValues = {
-    loader: false,
-    interestRate: 0.069,
-    monthlyPayment: 1358,
-    insurance: false,
-    insuranceAmount: 120,
-    arrangingFee: 0,
-    totalPayment: 118560
+	loader: false,
+	interestRate: 0.069,
+	monthlyPayment: 1358,
+	insurance: false,
+	insuranceAmount: 120,
+	arrangingFee: 0,
+	totalPayment: 118560
 }
 
 function paymentsReducer(payments = initialValues, action) {
-    switch (action.type) {
-        case "SHOW_LOADER":
-            return {
-                ...payments,
-                loader: true
-            }
-        case "HIDE_LOADER":
-            return {
-                ...payments,
-                loader: false
-            }
-        case "CALC":
-            const simpleCalc = (action.payload * 1.07) / 12
-            return {
-                ...payments,
-                monthlyPayments: simpleCalc
-            }
-        default:
-            return payments
-    }
-  }
+	switch (action.type) {
+		case 'SHOW_LOADER':
+			return {
+				...payments,
+				loader: true
+			}
+		case 'HIDE_LOADER':
+			return {
+				...payments,
+				loader: false
+			}
+		case 'CALC':
+			const simpleCalc = (action.payload * 1.07) / 12
+			return {
+				...payments,
+				monthlyPayments: simpleCalc
+			}
+		default:
+			return payments
+	}
+}
 
-  export default paymentsReducer
+export default paymentsReducer
--- redux/setAmounts.js
+++ redux/setAmounts.js
@@ -1,64 +1,64 @@
-// setting the minimum and maximum loan amount and the requested loan amount 
+// setting the minimum and maximum loan amount and the requested loan amount
 
 export function increment() {
-    return {
-        type: "INCREMENT",
-        payload: 100
-    }
+	return {
+		type: 'INCREMENT',
+		payload: 100
+	}
 }
 
 export function decrement() {
-    return {
-        type: "DECREMENT",
-        payload: 100
-    }
+	return {
+		type: 'DECREMENT',
+		payload: 100
+	}
 }
 
 export function setAmount(amount) {
-    return {
-        type: "SET_AMOUNT",
-        payload: amount
-    }
+	return {
+		type: 'SET_AMOUNT',
+		payload: amount
+	}
 }
 
 const initialValues = {
-    requested: 100000,
-    min: 20000,
-    max: 800000
+	requested: 100000,
+	min: 20000,
+	max: 800000
 }
 
 function amountReducer(amount = initialValues, action) {
-    switch (action.type) {
-        case "INCREMENT":
-            const sum = amount.requested + action.payload
-            return {
-                ...amount,
-                requested: sum <= amount.max ? sum : amount.requested
-            }
-        case "DECREMENT":
-            const diff = amount.requested - action.payload
-            return {
-                ...amount,
-                requested: diff >= amount.min ? diff : amount.requested
-            }
-        case "SET_AMOUNT":
-            return {
-                ...amount,
-                requested: action.payload
-            }
-        case "SET_MIN_AMOUNT":
-            return {
-                ...amount,
-                min: action.payload
-            }
-        case "SET_MAX_AMOUNT":
-            return {
-                ...amount,
-                max: action.payload
-            }
-        default:
-            return amount
-    }
-  }
+	switch (action.type) {
+		case 'INCREMENT':
+			const sum = amount.requested + action.payload
+			return {
+				...amount,
+				requested: sum <= amount.max ? sum : amount.requested
+			}
+		case 'DECREMENT':
+			const diff = amount.requested - action.payload
+			return {
+				...amount,
+				requested: diff >= amount.min ? diff : amount.requested
+			}
+		case 'SET_AMOUNT':
+			return {
+				...amount,
+				requested: action.payload
+			}
+		case 'SET_MIN_AMOUNT':
+			return {
+				...amount,
+				min: action.payload
+			}
+		case 'SET_MAX_AMOUNT':
+			return {
+				...amount,
+				max: action.payload
+			}
+		default:
+			return amount
+	}
+}
 
-  export default amountReducer
+export default amountReducer
