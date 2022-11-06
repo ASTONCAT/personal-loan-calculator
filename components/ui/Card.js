723c81d0d3b9a505b3dbd6acf2369e28b17b97bf
Koder Astoncat <koder@astoncat.com>
2022-11-06 09:49:56 +0100

    Fix layout issue on Setup page
    Add Rapid API variables into Environment Variables on Vercel

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,7 +1,13 @@
+import { useRouter } from 'next/router'
 import classes from './Card.module.css'
 
 function Card(props) {
-	return <div className={`${classes.card} ${classes.calculator}`}>{props.children}</div>
+	const router = useRouter()
+	const cardClasses =
+		router.pathname === '/setup'
+			? classes.card
+			: `${classes.card} ${classes.calculator}`
+	return <div className={cardClasses}>{props.children}</div>
 }
 
 export default Card
